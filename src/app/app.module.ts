35596041f66a9e98d00aa081576d81b766b5038e
Peter Nemere <peter.nemere@tattsgroup.com>
2018-07-20 17:07:05 +1000

    Regenerated project using Angular CLI 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ScheduleModule} from 'primeng/schedule';
 
 import { AppComponent } from './app.component';
 
-
+import {
+    ScheduleModule
+  } from 'primeng/primeng';
+  
 @NgModule({
   declarations: [
     AppComponent
